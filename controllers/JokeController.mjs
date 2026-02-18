72af0772bea37d0546125cddfd6509eeccb4d2ba
jacques <jacques@jdevelop.com>
2026-02-18 17:09:56 +0100

    Enhance validation for joke requests, update error handling in `show` method, and improve Swagger documentation.

--- controllers/JokeController.mjs
+++ controllers/JokeController.mjs
@@ -4,21 +4,15 @@ class JokeController {
     create(req, res) {
         if (req.body.joke === undefined) {
             res.status(400).json({'message': 'Joke is required'});
-        }
-        else if (Object.keys(req.body).length > 1) {
+        } else if (Object.keys(req.body).length > 1) {
             res.status(400).json({'message': 'Only joke object is allowed'});
-        }
-        else if (req.body.joke.question === undefined || typeof req.body.joke.question !== 'string') {
+        } else if (req.body.joke.question === undefined || typeof req.body.joke.question !== 'string') {
             res.status(400).json({'message': 'Joke question is required and must be a string'});
-        }
-        else if (req.body.joke.response === undefined || typeof req.body.joke.response !== 'string') {
+        } else if (req.body.joke.response === undefined || typeof req.body.joke.response !== 'string') {
             res.status(400).json({'message': 'Joke response is required and must be a string'});
-        }
-        else if (Object.keys(req.body.joke).length > 2) {
+        } else if (Object.keys(req.body.joke).length > 2) {
             res.status(400).json({'message': 'Only question and response are allowed in joke object'});
-        }
-        else
-        {
+        } else {
             jokes.create(req.body)
                 .then(joke => {
                     res.status(201).json({'message': 'Joke created successfully'})
@@ -34,15 +28,19 @@ class JokeController {
     }
 
     show(req, res) {
-        jokes.findByPk(req.params.id)
-            .then(joke => {
-                if (joke === null) {
-                    res.status(404).json({'message': 'Joke not found'})
-                } else {
-                    res.status(200).json(joke)
-                }
-            })
-            .catch(e => res.status(500).json({'message': 'Error retrieving joke'}))
+        if (req.params.id === undefined || isNaN(req.params.id)) {
+            res.status(400).json({'message': 'Joke ID is required and must be a number'})
+        } else {
+            jokes.findByPk(req.params.id)
+                .then(joke => {
+                    if (joke === null) {
+                        res.status(404).json({'message': 'Joke not found'})
+                    } else {
+                        res.status(200).json(joke)
+                    }
+                })
+                .catch(e => res.status(500).json({'message': 'Error retrieving joke'}))
+        }
     }
 
     random(req, res) {
